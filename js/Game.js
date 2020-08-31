3f887f28ad506e892e36e91f678d909bd441b0d1
damian08k <damiankierzkowski08@wp.pl>
2020-08-31 19:56:19 +0200

    create option to add players, number of them and start game

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,5 @@
-import RandomNumberGenerator from "./components/RandomNumberGenerator.js    ";
+import RandomNumberGenerator from "./components/RandomNumberGenerator.js";
+import Players from "./components/Players.js";
 
 class Game {
     constructor() {
@@ -9,15 +10,23 @@ class Game {
     initVariables() {
         // from HTML structure
         this.diceThrowBtn = document.querySelector(".player-info__dice-throw");
+
+        this.startGameBtn = document.querySelector(".start-game-window__start-game");
+        this.startGameWindow = document.querySelector(".start-game-window-container");
+        this.mainGameContainer = document.querySelector(".main-container");
+        this.firstPlayerName = document.querySelector(".stats__first-player");
+        this.secondPlayerName = document.querySelector(".stats__second-player");
+
         this.roundNumber = document.querySelector(".player-info__round-number");
         this.playerName = document.querySelector(".player-info__player-name");
         this.throws = [...document.querySelectorAll(".player-info__throw")];
 
         // instances of classes 
         this.randomNumberGenerator = new RandomNumberGenerator();
+        this.players = new Players();
 
         // other variables
-        this.players = [];
+        this.playersNames = [];
         this.diceClasses = [
             "fas fa-dice-one",
             "fas fa-dice-two",
@@ -29,7 +38,20 @@ class Game {
     }
 
     gameMainLogic() {
+        this.startGameBtn.addEventListener("click", this.startGame);
         this.diceThrowBtn.addEventListener("click", this.firstThrowDice);
+    }
+
+    startGame = () => {
+        const numberOfPlayers = parseInt(this.players.getNumberOfPlayers());
+        this.playersNames = this.players.getPlayersNames(numberOfPlayers);
+        if (this.playersNames.length !== 0) {
+            this.startGameWindow.style.display = "none";
+            // this.mainGameContainer.style.display = "block";
+            this.firstPlayerName.textContent = this.playersNames[0];
+            this.secondPlayerName.textContent = this.playersNames[1];
+            this.playerName.textContent = this.playersNames[0];
+        }
     }
 
     firstThrowDice = () => {
@@ -38,4 +60,4 @@ class Game {
     }
 }
 
-new Game();
+const game = new Game();
---
+++ js/components/Players.js
@@ -0,0 +1,62 @@
+export default class Players {
+    constructor() {
+        this.initPlayersVariables();
+        this.countPlayers();
+    }
+
+    initPlayersVariables() {
+        this.numberOfPlayers = document.querySelector(".choose-players__count-players");
+        this.plus = document.querySelector(".choose-players__plus-ico");
+        this.minus = document.querySelector(".choose-players__minus-ico");
+        this.firstPlayerName = document.querySelector("[data-player='one']");
+        this.secondPlayerElements = document.querySelectorAll("[data-player='two']");
+    }
+
+    addSecondPlayer(numberOfPlayers) {
+        if (numberOfPlayers === 2) {
+            this.secondPlayerElements.forEach(element => element.style.display = "block")
+        } else {
+            this.secondPlayerElements.forEach(element => element.style.display = "none")
+        }
+    }
+
+    changeNumberOfPlayers(sign, numberOfPlayers, addRemovePlayers) {
+        if (sign === addRemovePlayers[0]) {
+            numberOfPlayers < 2 ? numberOfPlayers++ : null;
+        } else {
+            numberOfPlayers > 1 ? numberOfPlayers-- : null;
+        }
+        this.numberOfPlayers.textContent = numberOfPlayers;
+
+        this.addSecondPlayer(numberOfPlayers);
+    }
+
+    countPlayers() {
+        let numberOfPlayers = 1;
+        const addRemovePlayers = [this.plus, this.minus];
+        addRemovePlayers.forEach(sign => {
+            sign.addEventListener("click", () => this.changeNumberOfPlayers(sign, numberOfPlayers, addRemovePlayers))
+        })
+    }
+
+    getNumberOfPlayers() {
+        return this.numberOfPlayers.textContent;
+    }
+
+    getPlayersNames(num) {
+        let names = [];
+        const firstPlayerName = this.firstPlayerName.value;
+        const secondPlayerName = (num === 2 ? this.secondPlayerElements[1].value : "Komputer");
+        if (firstPlayerName.length > 0 && firstPlayerName.length <= 8 && secondPlayerName.length > 0 && secondPlayerName.length <= 8) {
+            names.push(firstPlayerName, secondPlayerName);
+        } else {
+            // maybe special popup window instead of alert?
+            alert("Nazwa zawodnika powinna mieć od 1 do maksymalnie 8 znaków długości!");
+            this.firstPlayerName.value = "";
+            num === 2 ? this.secondPlayerElements[1].value = "" : null;
+        }
+
+        return names;
+    }
+
+}
